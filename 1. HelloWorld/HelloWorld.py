655bb3703b3d76a204c4ed88e28df86ba2156e15
Edilson de Souza <edilson.souza@gmail.com>
2020-05-10 18:23:35 -0300

    Adjusting comments

--- 1. HelloWorld/HelloWorld.py
+++ 1. HelloWorld/HelloWorld.py
@@ -1,14 +1,13 @@
 """
 ================================================================================
 Python Simple Code
- Writer: Edilson de Souza
- Date: April 10th, 2020
- Version: 0.1
+Writer: Edilson de Souza
+Date: April 10th, 2020
+Version: 0.1
 ================================================================================
 """
 
 'This is your first program in Python!'
 
 #Print command in Python
-
 print("Hello World!")
